9263ae2f0f056e761637942aaefd3d94b3c45884
Sahil Aggarwal <sahil.aggarwal@hyperverge.co>
2021-02-26 13:13:07 +0530

    Add show button on hover, Ui changes

--- src/components/chat/ChatComp.js
+++ src/components/chat/ChatComp.js
@@ -1,21 +1,21 @@
-import React from "react";
-import Paper from "@material-ui/core/Paper";
-import Conversations from "../../containers/chat/Conversations";
-import ConversationWindow from "../../containers/chat/ConversationWindow";
-import { makeStyles } from "@material-ui/core/styles";
-import ChatNavbar from "../../containers/chat/ChatNavbar";
-import { Grid, List, ListItem } from "@material-ui/core";
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Conversations from '../../containers/chat/Conversations';
+import ConversationWindow from '../../containers/chat/ConversationWindow';
+import { makeStyles } from '@material-ui/core/styles';
+import ChatNavbar from '../../containers/chat/ChatNavbar';
+import { Grid, List, ListItem } from '@material-ui/core';
 const useStyles = makeStyles({
   chatSection: {
-    width: "100%",
-    height: "80vh",
+    width: '100%',
+    height: '80vh',
   },
   window: {
-    border: "1px solid #cccccc",
+    border: '1px solid #cccccc',
     borderRadius: 10,
   },
 });
-const ChatComp = (props) => {
+const ChatComp = props => {
   const classes = useStyles();
   return (
     <div>
--- src/components/chat/ConversationWindowComp.js
+++ src/components/chat/ConversationWindowComp.js
@@ -1,13 +1,15 @@
-import React, { Fragment, useState } from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
-import moment from "moment";
-import Typography from "@material-ui/core/Typography";
-import EditIcon from "@material-ui/icons/Edit";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Cheese from "../layout/Cheese";
-import CheckIcon from "@material-ui/icons/Check";
-import DoneAllIcon from "@material-ui/icons/DoneAll";
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import moment from 'moment';
+import Typography from '@material-ui/core/Typography';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Cheese from '../layout/Cheese';
+import CheckIcon from '@material-ui/icons/Check';
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import BlockIcon from '@material-ui/icons/Block';
+import './hover.css';
 import {
   Grid,
   List,
@@ -19,44 +21,50 @@ import {
   Button,
   Dialog,
   DialogTitle,
-} from "@material-ui/core";
-import SendIcon from "@material-ui/icons/Send";
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 
 const useStyles = makeStyles({
   messageArea: {
-    height: "69vh",
-    overflowY: "auto",
-    border: "1px solid #f7f5f5",
+    height: '69vh',
+    overflowY: 'auto',
+    border: '1px solid #f7f5f5',
   },
   hoverMessage: {
     padding: 10,
-    marginRight: "10%",
-    marginTop: "1%",
-    background: "light",
+    marginRight: '10%',
+    marginTop: '1%',
+    background: 'light',
 
-    "&:hover": {
-      backgroundColor: "#f7f5f5",
+    '&:hover': {
+      backgroundColor: '#f7f5f5',
     },
   },
   editButton: {
-    fontSize: 5,
-    color: "green",
+    // fontSize: 5,
+    color: 'green',
   },
   deleteButton: {
-    fontSize: 5,
-    color: "red",
+    // fontSize: 5,
+    color: 'red',
   },
 
   time: {
-    position: "relative",
+    position: 'relative',
     top: 6,
   },
-  doubleIcon:{
-    color: "#005a9e",
+  doubleIcon: {
+    color: '#005a9e',
   },
-  singleCheckIcon:{
-    color: "#005a9e",
-  }
+  singleCheckIcon: {
+    color: '#005a9e',
+  },
+  deleteText: {
+    fontStyle: 'italic',
+  },
+  editText: {
+    fontSize: 12,
+  },
 });
 
 const ConversationWindowComp = ({
@@ -75,10 +83,10 @@ const ConversationWindowComp = ({
   const [openEdit, setOpenEdit] = React.useState({
     open: false,
     singleEvent: null,
-    editText: "",
+    editText: '',
   });
 
-  const handleClickOpenEdit = (singleEventProp) => {
+  const handleClickOpenEdit = singleEventProp => {
     console.log(singleEventProp.text);
     setOpenEdit({
       ...openEdit,
@@ -87,7 +95,7 @@ const ConversationWindowComp = ({
       editText: singleEventProp.text,
     });
   };
-  const onChangeEditText = (e) => {
+  const onChangeEditText = e => {
     console.log(openEdit.editText);
     setOpenEdit({ ...openEdit, editText: e.target.value });
   };
@@ -100,7 +108,7 @@ const ConversationWindowComp = ({
     singleEvent: null,
   });
 
-  const handleClickOpenDelete = (singleEvent) => {
+  const handleClickOpenDelete = singleEvent => {
     setOpenDelete({ ...openDelete, open: true, singleEvent: singleEvent });
   };
 
@@ -112,13 +120,13 @@ const ConversationWindowComp = ({
       <Fragment>
         <Grid item xs={12}>
           <List className={classes.messageArea}>
-            {events.map((event) => (
-              <Grid container className={classes.hoverMessage}>
+            {events.map(event => (
+              <Grid container className='message_area'>
                 <Grid item xs={9}>
                   <Grid container>
                     <Grid item xs={1}>
                       <ListItemText>
-                        <Typography gutterBottom variant="h7" component="h3">
+                        <Typography gutterBottom variant='h7' component='h3'>
                           {conversation.recipients[0]._id === event.sender
                             ? conversation.recipients[0].username
                             : conversation.recipients[1].username}
@@ -127,38 +135,67 @@ const ConversationWindowComp = ({
                     </Grid>
                     <Grid item xs={2}>
                       <ListItemText
-                        align="left"
+                        align='left'
                         className={classes.time}
-                        secondary={moment(event.date).format("LT")}
+                        secondary={moment(event.date).format('LT')}
                       ></ListItemText>
                     </Grid>
+                    {event.sender === auth.user._id &&
+                      event.type !== 'delete' && (
+                        <Fragment>
+                          {event.status == 'read' ? (
+                            <DoneAllIcon className={classes.doubleIcon} />
+                          ) : (
+                            <CheckIcon className={classes.singleCheckIcon} />
+                          )}
+                        </Fragment>
+                      )}
                   </Grid>
-                  <Typography gutterBottom variant="h7">
-                    {event.text}
-                  </Typography>
+
+                  {event.type === 'delete' ? (
+                    <Typography
+                      gutterBottom
+                      variant='h7'
+                      className={classes.deleteText}
+                    >
+                      <BlockIcon gutterBottom color="secondary"fontSize="small" />{event.text}
+                    </Typography>
+                  ) : event.type === 'new' ? (
+                    <Typography gutterBottom variant='h7' className=''>
+                      {event.text}
+                    </Typography>
+                  ) : (
+                    event.type === 'edit' && (
+                      <Typography gutterBottom variant='h7' className=''>
+                        {event.text}{' '}
+                        <Typography className={classes.editText}>
+                          {' '}
+                          (edited)
+                        </Typography>
+                      </Typography>
+                    )
+                  )}
                 </Grid>
                 <Grid item xs={2}>
-                  {event.sender === auth.user._id && (
+                  {event.sender === auth.user._id && event.type !== 'delete' && (
                     <Fragment>
-                      {event.status == "read" ? <DoneAllIcon className={classes.doubleIcon}/> : <CheckIcon className={classes.singleCheckIcon}/>}
                       <Grid container>
-                        <Grid item xs={1}>
+                        <Grid item xs={5}>
                           <ListItemText>
                             <Button
-                              className={classes.editButton}
+                              className='action'
                               onClick={() => handleClickOpenEdit(event)}
                             >
-                              <EditIcon />
+                              <EditIcon></EditIcon>
                             </Button>
                           </ListItemText>
                         </Grid>
-                        <Grid item xs={1}>
-                          <ListItemText>{"    "}</ListItemText>
-                        </Grid>
-                        <Grid item xs={1}>
+
+                        <br />
+                        <Grid item align='right' xs={6}>
                           <ListItemText>
                             <Button
-                              className={classes.deleteButton}
+                              className='action'
                               onClick={() => handleClickOpenDelete(event)}
                             >
                               <DeleteIcon />
@@ -173,11 +210,11 @@ const ConversationWindowComp = ({
             ))}
             <Dialog
               onClose={handleCloseDelete}
-              aria-labelledby="customized-dialog-title"
+              aria-labelledby='customized-dialog-title'
               open={openDelete.open}
             >
               <DialogTitle
-                id="customized-dialog-title"
+                id='customized-dialog-title'
                 onClose={handleCloseDelete}
               >
                 <h4>Delete this Message?</h4>
@@ -185,38 +222,40 @@ const ConversationWindowComp = ({
                 <Button
                   onClick={() => {
                     deleteEvent(openDelete.singleEvent);
+                    handleCloseDelete()
                   }}
                 >
-                  {" "}
+                  {' '}
                   Confirm
                 </Button>
               </DialogTitle>
             </Dialog>
             <Dialog
               onClose={handleCloseEdit}
-              aria-labelledby="customized-dialog-title"
+              aria-labelledby='customized-dialog-title'
               open={openEdit.open}
             >
               <DialogTitle
-                id="customized-dialog-title"
+                id='customized-dialog-title'
                 onClose={handleCloseEdit}
               >
                 Edit this Message
-                <Grid container style={{ padding: "12px" }}>
+                <Grid container style={{ padding: '12px' }}>
                   <Grid item xs={10}>
                     <TextField
-                      id="outlined-basic-email"
+                      id='outlined-basic-email'
                       fullWidth
                       value={openEdit.editText}
                       onChange={onChangeEditText}
                     ></TextField>
                   </Grid>
-                  <Grid xs={1} align="right">
+                  <Grid xs={1} align='right'>
                     <Fab
-                      color="primary"
-                      aria-label="add"
+                      color='primary'
+                      aria-label='add'
                       onClick={() => {
                         editEvent(openEdit.editText, openEdit.singleEvent);
+                        handleCloseEdit();
                       }}
                     >
                       <SendIcon />
@@ -230,18 +269,18 @@ const ConversationWindowComp = ({
           <Grid container>
             <Grid item xs={11}>
               <TextField
-                id="outlined-basic-email"
-                label="Type Something"
+                id='outlined-basic-email'
+                label='Type Something'
                 fullWidth
                 value={text}
                 onChange={onChange}
                 onKeyPress={showTyping}
               ></TextField>
             </Grid>
-            <Grid xs={1} align="right">
+            <Grid xs={1} align='right'>
               <Fab
-                color="primary"
-                aria-label="add"
+                color='primary'
+                aria-label='add'
                 onClick={() => {
                   onSubmit();
                 }}
